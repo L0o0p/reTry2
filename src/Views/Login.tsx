11f700e3c3b1d88ea082103375df1e21fb896e43
Loop Shen <2947232977@qq.com>
2024-01-23 23:47:27 +0800

    submit可以正确接收收入的信息

--- src/Views/Login.tsx
+++ src/Views/Login.tsx
@@ -1,111 +1,115 @@
 import React, { useState } from 'react';
-import { Button,  Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 
 const View = () => {
 
-// constructor(){
-//   super();
-//   this.state = {
-//     Usernam:"",
-//     password:"",
-//     repassword:"",
-//   }
-//   }
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //     Usernam:"",
+  //     password:"",
+  //     repassword:"",
+  //   }
+  //   }
 
 
-// const handleChange=(e)=>{
-//   this.setState({
-//     username:e.target.value
-//   })
-// }
+  // const handleChange=(e)=>{
+  //   this.setState({
+  //     username:e.target.value
+  //   })
+  // }
 
 
-// const onSubmit=()=>(
-//   // 禁用跳转：
-//   e.preventDefault(),
-// // 验证读取：
-//   console.log()
-//   )
-const handleSubmit = (e: { preventDefault: () => void; }) => {
-  e.preventDefault();
-  // 提交表单
-  console.log('Submitting form data:', FormData);
-};
+  // const onSubmit=()=>(
+  //   // 禁用跳转：
+  //   e.preventDefault(),
+  // // 验证读取：
+  //   console.log()
+  //   )
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    // 提交表单
+    console.log('Submitting form data:', FormData);
+  };
 
-type FieldType = {
-  username?: string;
-  password?: string;
-  // remember?: string;
-  repassword?: string;
-};
+  type FieldType = {
+    username?: string;
+    password?: string;
+    // remember?: string;
+    repassword?: string;
+  };
 
 
 
   const onFinish = (values: any) => {
     console.log('Success:', values);
   };
-  
+
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
   };
+
+  // 相当于构造器：constructor
   const [FormData, setFormData] = useState({
     username: '',
     password: '',
     repassword: '',
   });
 
+  // 输入框onChange事件
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { id, value } = e.target;
     setFormData(prevData => ({
       ...prevData,
-      [name]: value,
+      [id]: value,
     }));
+    console.log(e.target.id, ':', e.target.value)
   };
 
-  return(
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    // initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
+  return (
+    <Form
+      name=""
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      // initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item<FieldType>
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
       // onChange={handleChange}
-    >
-      <Input onChange={handleChange}/>
-    </Form.Item>
+      >
+        <Input onChange={handleChange} />
+      </Form.Item>
 
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password onChange={handleChange}/>
-    </Form.Item>
+      <Form.Item<FieldType>
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password onChange={handleChange} />
+      </Form.Item>
 
-    <Form.Item<FieldType>
-      label="rePassword"
-      name="repassword"
-      rules={[{ required: true, message: 'Please input your password again!' }]}
-    >
-      <Input.Password onChange={handleChange}/>
-    </Form.Item>
+      <Form.Item<FieldType>
+        label="rePassword"
+        name="repassword"
+        rules={[{ required: true, message: 'Please input your password again!' }]}
+      >
+        <Input.Password onChange={handleChange} />
+      </Form.Item>
 
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
 
 export default View;
