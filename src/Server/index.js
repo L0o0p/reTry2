53f10f7e1b615070af983869ba69ba91b3ec14a1
Loop <2947232977@qq.com>
2024-01-25 09:11:34 +0800

    修复一些接口测试问题

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,19 +1,20 @@
-
-
 // const express = require('express')
 // const router = require('/router')
 // const bodyparser = require("bodyparser")
-import express from 'express'
-import router from './router.js';
-import bodyparser from 'body-parser'
-const app = express()
+import express from "express";
+import router from "./router.js";
+import bodyparser from "body-parser";
+const app = express();
 
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(bodyparser.urlencoded({
-    extended: true
-}))
+app.use("/api", router);
 
 // 监听端口
 app.listen(5170, () => {
-    console.log('5170端口已监听')
-})
+  console.log("5170端口已监听");
+});
